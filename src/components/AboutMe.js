f75060b6c57fb63fadf35250ba57498a12e25373
Aidan Botha <aidanbotha30@gmail.com>
2024-07-30 17:36:30 +0200

    initial commit

---
+++ src/components/AboutMe.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutMe = () => {
+  return (
+    <section id="about">
+      <h1>About Me</h1>
+      <p>Hello! I'm a web developer with a passion for creating beautiful and functional websites...</p>
+    </section>
+  );
+};
+
+export default AboutMe;
