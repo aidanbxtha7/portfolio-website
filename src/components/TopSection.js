3b3d55ef5be19b6ed42826d76245a342f4e297c6
Aidan Botha <aidanbotha30@gmail.com>
2024-07-31 17:24:37 +0200

    DEVICE ADAPTATION

--- src/components/TopSection.js
+++ src/components/TopSection.js
@@ -6,7 +6,7 @@ const TopSection = ({ toggleTheme, theme }) => {
     <section className={`top-section ${theme}`}>
       <div className="top-section-content">
         <h1 className="hero-title">Hi, I'm Aidan</h1>
-        <p className="hero-subtitle">I'm a future software developer, passionate about innovating technology.</p>
+        <p className="hero-subtitle">I'm a software engineer and developer, passionate about innovating technology.</p>
         <div className="cta-buttons">
           <a href="#resume" className="cta-button">Resume</a>
           <a href="#contact" className="cta-button">Contact Me</a>
